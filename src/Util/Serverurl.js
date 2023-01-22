8273e1ac65e365bbcc2c53047abf9cd2fd11cb5d
Sanskar Singh <sanskarsingh8176@gmail.com>
2023-01-22 18:19:30 +0530

    added server url

--- src/Util/Serverurl.js
+++ src/Util/Serverurl.js
@@ -1,1 +1,5 @@
-export const API_URL = "http://localhost:5000";
+
+// local 
+// export const API_URL = "http://localhost:5000";
+//deployed
+export const API_URL = "https://simplechat-api.onrender.com";
